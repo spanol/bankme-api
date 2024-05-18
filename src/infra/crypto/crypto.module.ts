724f21ac7a8dec2bbd53c3e0b5b7871bba5bc1ea
spanol <viniciusspanol@gmail.com>
2024-05-18 15:59:10 -0300

    chore: Add bcrypt dependency and implement hash generator

--- src/infra/crypto/crypto.module.ts
+++ src/infra/crypto/crypto.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { BCryptService } from './bcrypt/bcrypt.service';
+import { HashGenerator } from './shared/hash/hash-generator';
 
-@Module({})
+@Module({
+  providers: [
+    {
+      provide: HashGenerator,
+      useClass: BCryptService,
+    },
+  ],
+  exports: [HashGenerator],
+})
 export class CryptoModule {}
