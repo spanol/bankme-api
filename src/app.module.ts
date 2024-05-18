ee0c26d20792458516fac529c2ff9027da4e51f9
spanol <viniciusspanol@gmail.com>
2024-05-18 15:52:10 -0300

    chore: Rearanged folder structure for modularity

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HttpModule } from './infra/http/http.module';
+import { DatabaseModule } from './infra/database/database.module';
+import { CryptoModule } from './infra/crypto/crypto.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HttpModule,
+    DatabaseModule,
+    CryptoModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
 })
 export class AppModule {}
