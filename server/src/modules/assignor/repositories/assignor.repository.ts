bd042acbb8057a0030c41c2961d7e351f091dcb0
spanol <viniciusspanol@gmail.com>
2024-05-19 16:14:22 -0300

    refactor: Add findByDocument method to AssignorRepository and findByLogin method to UserRepository

--- server/src/modules/assignor/repositories/assignor.repository.ts
+++ server/src/modules/assignor/repositories/assignor.repository.ts
@@ -1,4 +1,6 @@
 import { GenericRepository } from '@infra/database/shared/generic-repository';
 import { Assignor } from '@prisma/client';
 
-export abstract class AssignorRepository extends GenericRepository<Assignor> {}
+export abstract class AssignorRepository extends GenericRepository<Assignor> {
+  abstract findByDocument(document: string): Promise<Assignor | null>;
+}
--- server/src/modules/user/repositories/user.repository.ts
+++ server/src/modules/user/repositories/user.repository.ts
@@ -1,4 +1,6 @@
 import { GenericRepository } from '@infra/database/shared/generic-repository';
 import { User } from '../entities/user.entity';
 
-export abstract class UserRepository extends GenericRepository<User> {}
+export abstract class UserRepository extends GenericRepository<User> {
+  abstract findByLogin(login: string): Promise<User | null>;
+}
