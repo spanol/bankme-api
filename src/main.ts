2e8fc37ef2a2d505e387fe5143a3cd2386fbf514
spanol <viniciusspanol@gmail.com>
2024-05-18 21:04:40 -0300

    chore: Update dependencies and add Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+import { useSwagger } from '@infra/http/swagger/useSwagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+  useSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
