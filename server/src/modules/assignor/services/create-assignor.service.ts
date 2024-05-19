81efc24525403c3e91a9890a321dd71a06484f16
spanol <viniciusspanol@gmail.com>
2024-05-19 16:14:34 -0300

    refactor: Add unit test for CreateAssignorService

--- server/src/modules/assignor/services/create-assignor.service.ts
+++ server/src/modules/assignor/services/create-assignor.service.ts
@@ -2,23 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { AssignorRepository } from '../repositories/assignor.repository';
 import { CreateAssignorDto } from '@infra/http/assignor/dtos/create-assignor.dto';
 import { Assignor } from '../entities/assignor.entity';
+import { Either, right } from '@utils/either';
 
+type CreateAssignorServiceResponse = Either<Error, Assignor>;
 @Injectable()
 export class CreateAssignorService {
   constructor(private repository: AssignorRepository) {}
 
-  async execute({ id, document, email, name, phone }: CreateAssignorDto) {
+  async execute({
+    document,
+    email,
+    name,
+    phone,
+  }: CreateAssignorDto): Promise<CreateAssignorServiceResponse> {
     const assignor = Assignor.create({
-      id,
-      payables: [],
       document,
+      payables: [],
       email,
       name,
       phone,
     });
 
+    const assignorAlreadyExists =
+      await this.repository.findByDocument(document);
+
+    if (assignorAlreadyExists) {
+      return right(assignorAlreadyExists);
+    }
+
     await this.repository.create(assignor);
 
-    return assignor;
+    return right(assignor);
   }
 }
