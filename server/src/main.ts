42907198ee62f19a300d945b60f810e511f6ff9b
spanol <viniciusspanol@gmail.com>
2024-05-20 05:49:39 -0300

    refactor: Enable CORS with specified origin and methods

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,12 +3,14 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { useSwagger } from '@infra/http/swagger/useSwagger';
 import * as passport from 'passport';
+import { useCors } from '@infra/http/cors/useCors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  useSwagger(app);
   app.use(passport.initialize());
+  useCors(app);
+  useSwagger(app);
   await app.listen(3000);
 }
 bootstrap();
