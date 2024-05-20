42ad97b2d7541fa9dc10f896c55041f3152c9b40
spanol <viniciusspanol@gmail.com>
2024-05-20 05:51:54 -0300

    refactor: Update useSwagger to add bearer authentication with JWT scheme

--- server/src/infra/http/swagger/useSwagger.ts
+++ server/src/infra/http/swagger/useSwagger.ts
@@ -5,7 +5,10 @@ export const useSwagger = (app: INestApplication): void => {
   const options = new DocumentBuilder()
     .setTitle('bankme challenge api')
     .setVersion('0.1')
-    .addBearerAuth()
+    .addBearerAuth(
+      { type: 'http', scheme: 'bearer', bearerFormat: 'JWT' },
+      'jwt',
+    )
     .build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
