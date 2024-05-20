c11551827a039b428e3f64e15c8428d17858be92
spanol <viniciusspanol@gmail.com>
2024-05-20 05:47:47 -0300

    refactor: Update app component to import HeaderComponent and add CSS layout class

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,19 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PayableFormComponent } from './payables/payable-form/payable-form.component';
-import { PayableListComponent } from './payables/payable-list/payable-list.component';
-import { PayableDetailsComponent } from './payables/payable-details/payable-details.component';
-import { AuthGuard } from './auth/auth.guard';
-import { LoginComponent } from './auth/login/login.component';
+import { AuthGuard } from './modules/auth/auth.guard';
+import { LoginComponent } from './modules/auth/login/login.component';
+import { PayableDetailsComponent } from './modules/payables/payable-details/payable-details.component';
+import { PayableFormComponent } from './modules/payables/payable-form/payable-form.component';
+import { PayableListComponent } from './modules/payables/payable-list/payable-list.component';
+import { AssignorListComponent } from './modules/assignors/assignor-list/assignor-list.component';
+import { AssignorDetailsComponent } from './modules/assignors/assignor-details/assignor-details.component';
+import { AssignorFormComponent } from './modules/assignors/assignor-form/assignor-form.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/payables', pathMatch: 'full' },
+  // wildcard route prevent 404
   {
     path: 'login',
     component: LoginComponent,
   },
-  // assignor
-
+  {
+    path: 'assignors',
+    component: AssignorListComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'assignors/new',
+    component: AssignorFormComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'assignors/:id',
+    component: AssignorDetailsComponent,
+    canActivate: [AuthGuard],
+  },
   {
     path: 'payables',
     component: PayableListComponent,
@@ -29,6 +46,7 @@ export const routes: Routes = [
     component: PayableDetailsComponent,
     canActivate: [AuthGuard],
   },
+  { path: '**', redirectTo: '/payables' },
 ];
 
 @NgModule({
