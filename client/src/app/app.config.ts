d617d0594d7a7ef138cdc1b1b137d4871c48905f
spanol <viniciusspanol@gmail.com>
2024-05-21 02:14:27 -0300

    refactor: Update lazy loading configuration for assignors and payables routes

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,12 +1,17 @@
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  PreloadAllModules,
+  provideRouter,
+  withPreloading,
+} from '@angular/router';
 
-import { routes } from './app.routes';
+import { APP_ROUTES } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { TokenInterceptor } from './interceptors/token-interceptor';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideHttpClient(
-    withInterceptors([ TokenInterceptor ])
-  )]
+  providers: [
+    provideRouter(APP_ROUTES, withPreloading(PreloadAllModules)),
+    provideHttpClient(withInterceptors([TokenInterceptor])),
+  ],
 };
--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,55 +2,34 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './modules/auth/auth.guard';
 import { LoginComponent } from './modules/auth/login/login.component';
-import { PayableDetailsComponent } from './modules/payables/payable-details/payable-details.component';
-import { PayableFormComponent } from './modules/payables/payable-form/payable-form.component';
-import { PayableListComponent } from './modules/payables/payable-list/payable-list.component';
-import { AssignorListComponent } from './modules/assignors/assignor-list/assignor-list.component';
-import { AssignorDetailsComponent } from './modules/assignors/assignor-details/assignor-details.component';
-import { AssignorFormComponent } from './modules/assignors/assignor-form/assignor-form.component';
 
-export const routes: Routes = [
+export const APP_ROUTES: Routes = [
   { path: '', redirectTo: '/payables', pathMatch: 'full' },
-  // wildcard route prevent 404
   {
     path: 'login',
     component: LoginComponent,
   },
   {
+    canActivate: [AuthGuard],
     path: 'assignors',
-    component: AssignorListComponent,
-    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./modules/assignors/assignors.routes').then(
+        (m) => m.ASSIGNORS_ROUTES
+      ),
   },
   {
-    path: 'assignors/new',
-    component: AssignorFormComponent,
     canActivate: [AuthGuard],
-  },
-  {
-    path: 'assignors/:id',
-    component: AssignorDetailsComponent,
-    canActivate: [AuthGuard],
-  },
-  {
     path: 'payables',
-    component: PayableListComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'payables/new',
-    component: PayableFormComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'payables/:id',
-    component: PayableDetailsComponent,
-    canActivate: [AuthGuard],
+    loadChildren: () =>
+      import('./modules/payables/payables.routes').then(
+        (m) => m.PAYABLES_ROUTES
+      ),
   },
   { path: '**', redirectTo: '/payables' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(APP_ROUTES)],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
