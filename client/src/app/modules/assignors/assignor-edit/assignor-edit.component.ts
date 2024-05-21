c706defb0cb5609048b0f6093d746a5e52722a79
spanol <viniciusspanol@gmail.com>
2024-05-21 02:17:35 -0300

    refactor: Add assignor-edit component for editing assignor details

---
+++ client/src/app/modules/assignors/assignor-edit/assignor-edit.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-assignor-edit',
+  standalone: true,
+  imports: [],
+  templateUrl: './assignor-edit.component.html',
+  styleUrl: './assignor-edit.component.css'
+})
+export class AssignorEditComponent {
+
+}
