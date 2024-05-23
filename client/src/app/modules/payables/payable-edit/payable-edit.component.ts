b324fa1639598eb19c3aaab1eebf4071a013727e
spanol <viniciusspanol@gmail.com>
2024-05-23 02:00:17 -0300

    refactor: Update payable-edit component to include form validation and error handling

--- client/src/app/modules/payables/payable-edit/payable-edit.component.ts
+++ client/src/app/modules/payables/payable-edit/payable-edit.component.ts
@@ -2,13 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { PayableModel } from '../../../models/payable.model';
 import { PayableService } from '../payable.service';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AlertsService } from '../../alerts/alerts.service';
+import { CommonModule } from '@angular/common';
+import { NgxCurrencyDirective } from 'ngx-currency';
 
 @Component({
   selector: 'app-payable-edit',
   standalone: true,
-  imports: [RouterModule],
+  imports: [
+    RouterModule,
+    CommonModule,
+    NgxCurrencyDirective,
+    ReactiveFormsModule,
+    FormsModule,
+  ],
   templateUrl: './payable-edit.component.html',
   styleUrl: './payable-edit.component.css',
 })
@@ -20,22 +34,68 @@ export class PayableEditComponent implements OnInit {
     private fb: FormBuilder,
     private payableService: PayableService,
     private route: ActivatedRoute,
+    private router: Router,
     private alertsService: AlertsService
-  ) {}
+  ) {
+    document.title = 'Payable - Edit';
+  }
 
   ngOnInit() {
     this.payableForm = this.fb.group({
-
       value: ['', [Validators.required, Validators.min(0)]],
       emissionDate: ['', Validators.required],
-      email: ['', [Validators.required, Validators.email]],
+      assignorName: ['', Validators.required],
+      assignorEmail: ['', [Validators.required, Validators.email]],
+      assignorId: ['', Validators.required],
     });
 
     const id = this.route.snapshot.paramMap.get('id');
     this.payableService.getPayableById(id as string).subscribe((data) => {
       this.payable = data;
-      this.payableForm.patchValue(this.payable);
+      this.payableForm.patchValue({
+        value: this.payable.value,
+        emissionDate: this.payable.emissionDate,
+        assignorName: this.payable.assignor.name,
+        assignorEmail: this.payable.assignor.email,
+        assignorId: this.payable.assignor.id,
+      });
       console.log(this.payableForm.value);
     });
   }
+
+  onCancel() {
+    this.payableForm.reset();
+    this.router.navigate(['/payables']);
+  }
+
+
+  get value() {
+    return this.payableForm.get('value');
+  }
+
+  get emissionDate() {
+    return this.payableForm.get('emissionDate');
+  }
+
+  get email() {
+    return this.payableForm.get('email');
+  }
+
+  get assignorId() {
+    return this.payableForm.get('assignorId');
+  }
+
+  onSubmit() {
+    if (this.payableForm.valid) {
+      // this.payableService.updatePayable(this.payableForm.value).subscribe({
+      //   next: () => {
+      //     this.alertsService.showSuccess('Payable updated successfully');
+      //   },
+      //   error: () => {
+      //     this.alertsService.showError('Failed to update payable');
+      //   },
+      // });
+    }
+  }
+
 }
