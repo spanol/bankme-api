ab4338737c9a254dde8341c91d8e76e8660078b9
spanol <viniciusspanol@gmail.com>
2024-05-23 02:01:08 -0300

    refactor: Update phone pipe to handle null or undefined values

--- client/src/app/pipes/phone.pipe.ts
+++ client/src/app/pipes/phone.pipe.ts
@@ -4,7 +4,7 @@ import { formatPhoneNumber } from '../helpers/format-assignor-phone';
 @Pipe({ name: 'phone', standalone: true })
 export class PhonePipe implements PipeTransform {
   transform(value: string | number): string {
-    let phone = value.toString();
+    let phone = value?.toString();
     return formatPhoneNumber(phone);
   }
 }
