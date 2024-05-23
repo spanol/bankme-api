a39bf7b9222e1a3b98c4c19bb1aa8d010c52a937
spanol <viniciusspanol@gmail.com>
2024-05-23 01:59:26 -0300

    refactor: Update assignor list component to include pagination controls

--- client/src/app/modules/assignors/assignor-list/assignor-list.component.ts
+++ client/src/app/modules/assignors/assignor-list/assignor-list.component.ts
@@ -4,26 +4,29 @@ import { Router, RouterModule } from '@angular/router';
 import { AssignorService } from '../assignor.service';
 import { AssignorModel } from '../../../models/assignor.model';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-assignor-list',
   templateUrl: './assignor-list.component.html',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgxPaginationModule],
   styleUrls: ['./assignor-list.component.scss'],
 })
 export class AssignorListComponent implements OnInit {
   assignors: AssignorModel[] = [];
+  actualPage: number = 1;
 
   constructor(
     private assignorService: AssignorService,
     private router: Router
-  ) {}
+  ) {
+    document.title = 'Assignors - List';
+  }
 
   ngOnInit() {
     this.assignorService.getAssignors().subscribe((data) => {
       this.assignors = data;
-      console.log(this.assignors);
     });
   }
 
