c6b683abc508d9f582938ea80a17d6db718e92bb
spanol <viniciusspanol@gmail.com>
2024-05-23 02:00:33 -0300

    refactor: Update login component to use reactive forms for input validation

--- client/src/app/modules/auth/login/login.component.ts
+++ client/src/app/modules/auth/login/login.component.ts
@@ -1,26 +1,63 @@
-import { Component } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { AlertsService } from '../../alerts/alerts.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, FormsModule],
+  imports: [ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
-  credentials = { login: '', password: '' };
+export class LoginComponent implements OnInit {
+  form: FormGroup = new FormGroup({});
 
-  constructor(private authService: AuthService, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private alertService: AlertsService
+  ) {
     document.title = 'Login';
   }
 
-  onSubmit() {
-    console.log(this.credentials);
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      login: '',
+      password: '',
+    });
+  }
 
-    this.authService.login(this.credentials).subscribe({
+  get login() {
+    return this.form.get('login');
+  }
+
+  get password() {
+    return this.form.get('password');
+  }
+
+  onSubmit() {
+    console.log(this.form);
+
+    if (
+      !this.form.value.login ||
+      !this.form.value.password ||
+      this.form.value.login === '' ||
+      this.form.value.password === ''
+    ) {
+      this.alertService.error('Login and password are required');
+      return;
+    }
+
+    this.authService.login(this.form.value).subscribe({
       next: (response) => {
         this.authService.setToken(response.access_token);
       },
@@ -30,7 +67,7 @@ export class LoginComponent {
       },
       error: (error) => {
         console.error(error);
-      }
+      },
     });
   }
 }
