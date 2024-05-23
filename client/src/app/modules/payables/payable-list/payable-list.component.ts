4d9e14cb3698721ac0157c0acdc93e539a36658b
spanol <viniciusspanol@gmail.com>
2024-05-23 02:01:18 -0300

    refactor: Update payable list component to include pagination controls and improve UI

--- client/src/app/modules/payables/payable-list/payable-list.component.ts
+++ client/src/app/modules/payables/payable-list/payable-list.component.ts
@@ -3,18 +3,22 @@ import { PayableService } from '../payable.service';
 import { PayableModel } from '../../../models/payable.model';
 import { Router, RouterModule } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-payable-list',
   templateUrl: './payable-list.component.html',
   standalone: true,
-  imports: [RouterModule, CommonModule],
+  imports: [RouterModule, CommonModule, NgxPaginationModule],
   styleUrl: './payable-list.component.css',
 })
 export class PayableListComponent implements OnInit {
   payables: PayableModel[] = [];
+  actualPage: number = 1;
 
-  constructor(private payableService: PayableService, private router: Router) {}
+  constructor(private payableService: PayableService, private router: Router) {
+    document.title = 'Payable - list';
+  }
 
   ngOnInit() {
     this.payableService.getPayables().subscribe((data) => {
@@ -28,7 +32,7 @@ export class PayableListComponent implements OnInit {
     this.router.navigate(['payables', 'edit', id]);
   }
 
-  deletePayable(ev: Event,id: string) {
+  deletePayable(ev: Event, id: string) {
     // prevent event buble
     ev.stopPropagation();
     this.payableService.deletePayable(id).subscribe(() => {
